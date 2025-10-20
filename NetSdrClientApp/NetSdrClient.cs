9286e76b1d4a2a3337bb07d494d2d72a33258a0f
suprunnnn <8562206@stud.kai.edu.ua>
2025-10-20 22:27:14 +0300

    Update NetSdrClient.cs

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -66,7 +66,7 @@ namespace NetSdrClientApp
                 return;
             }
 
-;           var iqDataMode = (byte)0x80;
+            var iqDataMode = (byte)0x80;
             var start = (byte)0x02;
             var fifo16bitCaptureMode = (byte)0x01;
             var n = (byte)1;
